da2a4b183bf1781f2869d33639debb69f6f2f74b
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-12-23 14:33:23 +0300

    Семинары (список)

---
+++ scripts/scripts.js
@@ -0,0 +1,46 @@
+/* Slider on main page */
+
+function slider() {
+  for (var i = 0; i < $(".slider-pagination__item").length; i++) {
+    if ($($(".slider-pagination__item")[i]).hasClass("slider-pagination__item--active")) {
+      if (i == 4) {
+        $($(".slider-pagination__item")[i]).removeClass("slider-pagination__item--active");
+        $($(".slider-pagination__item")[0]).addClass("slider-pagination__item--active");
+        $($(".slider-content__item")[i]).removeClass("slider-content__item--active");
+        $($(".slider-content__item")[0]).addClass("slider-content__item--active");
+        break;
+      } else {
+        $($(".slider-pagination__item")[i]).removeClass("slider-pagination__item--active");
+        $($(".slider-pagination__item")[i + 1]).addClass("slider-pagination__item--active");
+        $($(".slider-content__item")[i]).removeClass("slider-content__item--active");
+        $($(".slider-content__item")[i + 1]).addClass("slider-content__item--active");
+        break;
+      }
+    }
+  }
+};
+
+setInterval(slider, 3000);
+
+
+$(".slider-pagination__item").click(function(e) {
+  e.preventDefault();
+  for (var i = 0; i < $(".slider-pagination__item").length; i++) {
+    $($(".slider-pagination__item")[i]).removeClass("slider-pagination__item--active");
+    $($(".slider-content__item")[i]).removeClass("slider-content__item--active");
+  }
+  $(this).addClass("slider-pagination__item--active");
+  var num = $(this).attr("class").charAt(49);
+  $($(".slider-content__item")[num - 1]).addClass("slider-content__item--active");
+});
+
+
+/* Seminars list */
+
+$(".seminars-list__item").mouseover(function() {
+  $(this).addClass("seminars-list__item--active");
+});
+
+$(".seminars-list__item").mouseout(function() {
+  $(this).removeClass("seminars-list__item--active");
+});
